fe2a76e8067d6754fdd2a7ef32c59679eca56b28
William Chow <431741580derp@gmail.com>
2019-12-13 09:36:51 +1100

    Make the test script print individual items

--- test.cpp
+++ test.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include "yaml-cpp/yaml.h"
-
+#include <string>
 
 int main(int argc, char const *argv[]) {
 	std::ifstream testfile("test.yaml");
 	YAML::Node mine_template = YAML::LoadFile("test.yaml");
+	std::cout << "whole: " << mine_template << "\n";
+	std::cout << "Partial: \n";
+	for (auto r : mine_template) {
+		std::cout << r.first.as<std::string>() << "\n";
+	}
 	YAML::Node panel = mine_template["Solar Panel II"];
 	std::cout << "type: " << panel.Type() << "\n";
 	std::cout << "type: " << panel["ingredients"].Type() << "\n";
